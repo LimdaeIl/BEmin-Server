837d309356daa6439f0c64e06dd53662a641379f
heejinKong <gmlwls3379@naver.com>
2025-02-23 22:06:51 +0900

    Feat: UserController 테스트 코드 추가
    - MockConfig에 AuthService, UserService, UserAddressService의 Mockito.mock 빈 추가
    - UserControllerTest에서 MockConfig 및 TestSecurityConfig를 Import하여, 컨트롤러 테스트에 필요한 서비스 빈 제공
    - 관련 테스트 케이스(User 조회, 수정, 삭제, 주소 조회 및 추가 등)에서 모의 객체를 활용하여 정상 동작 확인

--- src/test/java/run/bemin/api/config/MockConfig.java
+++ src/test/java/run/bemin/api/config/MockConfig.java
@@ -4,11 +4,23 @@ import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import run.bemin.api.auth.service.AuthService;
+import run.bemin.api.user.service.UserAddressService;
+import run.bemin.api.user.service.UserService;
 
 @TestConfiguration
 public class MockConfig {
   @Bean
   public AuthService authService() {
     return Mockito.mock(AuthService.class);
+  }
+
+  @Bean
+  public UserService userService() {
+    return Mockito.mock(UserService.class);
+  }
+
+  @Bean
+  public UserAddressService userAddressService() {
+    return Mockito.mock(UserAddressService.class);
   }
 }
