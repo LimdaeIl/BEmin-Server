cd778aa031a60bd312408d3f7d504507d364cc0b
heejinKong <gmlwls3379@naver.com>
2025-02-24 16:55:04 +0900

    Feat(auth): AuthSessionController 테스트 케이스 작성 및 MockConfig 업데이트
    - 로그인, 리프레시 토큰, 로그아웃 성공 테스트 케이스를 AuthSessionControllerTest에 작성
    - 테스트 실행을 위해 MockConfig에 JwtUtil 및 RedisTemplate Bean 추가

--- src/test/java/run/bemin/api/config/MockConfig.java
+++ src/test/java/run/bemin/api/config/MockConfig.java
@@ -3,6 +3,8 @@ package run.bemin.api.config;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.core.RedisTemplate;
+import run.bemin.api.auth.jwt.JwtUtil;
 import run.bemin.api.auth.service.AuthService;
 import run.bemin.api.user.service.UserAddressService;
 import run.bemin.api.user.service.UserService;
@@ -12,6 +14,16 @@ public class MockConfig {
   @Bean
   public AuthService authService() {
     return Mockito.mock(AuthService.class);
+  }
+
+  @Bean
+  public JwtUtil jwtUtil() {
+    return Mockito.mock(JwtUtil.class);
+  }
+
+  @Bean
+  public RedisTemplate redisTemplate() {
+    return Mockito.mock(RedisTemplate.class);
   }
 
   @Bean
