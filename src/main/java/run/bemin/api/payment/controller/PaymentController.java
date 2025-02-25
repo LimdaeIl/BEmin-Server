665b840211a6ea3b789a208e65fcd0092ae8a17e
kimseonhee126 <kimseonhee126@gmail.com>
2025-02-25 12:51:49 +0900

    Refactor: Swagger 문서 작성하기 Payment, Review 도메인에서 권한 부여가 필요한 부분 어노테이션 추가 Payment, Review 도메인에서 Swagger 문서 작성하기

--- src/main/java/run/bemin/api/payment/controller/PaymentController.java
+++ src/main/java/run/bemin/api/payment/controller/PaymentController.java
@@ -1,5 +1,6 @@
 package run.bemin.api.payment.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import java.util.UUID;
@@ -7,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +37,9 @@ public class PaymentController {
   private final PaymentService paymentService;
   private final UserService userService;
 
+  @PreAuthorize("hasRole('MASTER') or hasRole('MANAGER')")
   @GetMapping("/payments/status")
+  @Operation(summary = "결제 상태 조회", description = "결제 ID에 대해 결제 상태 조회")
   public ResponseEntity<ApiResponse<PaymentStatusResponseDto>> getPaymentStatus(@RequestParam UUID paymentId) {
     PaymentStatusResponseDto response = paymentService.getPaymentStatus(paymentId);
 
@@ -43,7 +47,9 @@ public class PaymentController {
   }
 
   @GetMapping("/user/payments")
-  public ResponseEntity<ApiResponse<List<PaymentDto>>> getUserPayments(UserDetailsImpl userDetails) {
+  @Operation(summary = "사용자의 결제 내역 조회", description = "특정 사용자의 모든 결제 내역 조회")
+  public ResponseEntity<ApiResponse<List<PaymentDto>>> getUserPayments(
+      @AuthenticationPrincipal UserDetailsImpl userDetails) {
     User user = userService.findByUserEmail(userDetails.getUsername());
 
     List<PaymentDto> payments = paymentService.getUserPayments(user);
@@ -52,6 +58,7 @@ public class PaymentController {
   }
 
   @PostMapping("/payments")
+  @Operation(summary = "결제 생성하기", description = "주문이 완료되었는지 검사 후, 결제 진행하기")
   public ResponseEntity<ApiResponse<PaymentDto>> createPayment(@RequestBody CreatePaymentDto createPaymentDto) {
     PaymentDto payment = paymentService.createPayment(createPaymentDto);
 
@@ -59,6 +66,7 @@ public class PaymentController {
   }
 
   @DeleteMapping("/payments/cancel")
+  @Operation(summary = "결제 취소하기", description = "결제 취소하기")
   public ResponseEntity<ApiResponse<PaymentCancelDto>> deletePayment(
       @AuthenticationPrincipal UserDetailsImpl userDetails,
       @RequestParam UUID paymentId) {
