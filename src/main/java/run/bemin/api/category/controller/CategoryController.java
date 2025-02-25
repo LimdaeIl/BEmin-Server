395eed5e2564e66c062f0f52c52806fa97c44b33
LimDae94 <piay801@naver.com>
2025-02-25 13:11:24 +0900

    Feat: #135 카테고리 자바독, 스웨거 구현하기

--- src/main/java/run/bemin/api/category/controller/CategoryController.java
+++ src/main/java/run/bemin/api/category/controller/CategoryController.java
@@ -3,6 +3,8 @@ package run.bemin.api.category.controller;
 
 import static run.bemin.api.category.dto.CategoryResponseCode.CATEGORIES_FETCHED;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,10 @@ import run.bemin.api.category.dto.response.GetCategoryResponseDto;
 import run.bemin.api.category.service.CategoryService;
 import run.bemin.api.general.response.ApiResponse;
 
+/**
+ * The type Category controller.
+ */
+@Tag(name = "[사용자]카테고리", description = "CategoryController")
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/categories")
 @RestController
@@ -22,7 +28,20 @@ public class CategoryController {
 
   private final CategoryService categoryService;
 
-
+  /**
+   * Gets all categories.
+   *
+   * @param name the name
+   * @param page the page
+   * @param size the size
+   * @return the all categories
+   */
+  @Operation(summary = "[사용자] 카테고리 조회하기(삭제 제외)",
+      description = """
+          name(선택) : 카테고리 이름은 한글, 숫자, 특수 문자(·, !), 공백만 입력 가능하며, 1~16글자 이내여야 합니다. \n
+          page(선택) : 조회할 페이지 번호(defaultValue = 0) \n
+          size(선택) : 한 페이지에 포함될 항목 수(defaultValue = 10)
+          """)
   @PreAuthorize("hasRole('CUSTOMER') or hasRole('MANANGER') or hasRole('MASTER') or hasRole('OWNER')")
   @GetMapping
   public ResponseEntity<ApiResponse<Page<GetCategoryResponseDto>>> getAllCategories(
