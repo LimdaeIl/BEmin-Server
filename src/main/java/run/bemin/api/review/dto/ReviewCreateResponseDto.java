2086295130bbfd727dd7126140e9a86a36a3ec62
LimDae94 <piay801@naver.com>
2025-02-25 11:27:34 +0900

    Fix: #128 ReviewCreateResponseDto 올바르게 수정하기
    getReviewId 잘못된 매핑으로 연결 된 것을 다시 올바르게 수정하기
    @Setter 추가하기

--- src/main/java/run/bemin/api/review/dto/ReviewCreateResponseDto.java
+++ src/main/java/run/bemin/api/review/dto/ReviewCreateResponseDto.java
@@ -1,5 +1,6 @@
 package run.bemin.api.review.dto;
 
+import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
 import run.bemin.api.review.domain.ReviewRating;
@@ -11,16 +12,22 @@ public class ReviewCreateResponseDto {
   private String paymentId;
   private String orderId;
   private String storeId;
+  private String reviewId;
   private ReviewRating reviewRating;
   private String description;
+  private String createdBy;
+  private LocalDateTime createdAt;
 
   public static ReviewCreateResponseDto from(Review review) {
     return ReviewCreateResponseDto.builder()
         .paymentId(String.valueOf(review.getPayment().getPaymentId()))
         .orderId(String.valueOf(review.getOrder().getOrderId()))
-        .storeId(String.valueOf(review.getReviewId()))
+        .storeId(String.valueOf(review.getStore().getId()))
+        .reviewId(String.valueOf(review.getReviewId()))
         .reviewRating(review.getRating())
         .description(review.getDescription())
+        .createdBy(review.getCreatedBy())
+        .createdAt(review.getCreatedAt())
         .build();
   }
 }
