a855d405879fe800ce600f922d6ec87d95e0cb07
heejinKong <gmlwls3379@naver.com>
2025-02-25 12:53:19 +0900

    Docs : Auth와 User 도메인 Swagger & JavaDocs 문서화 추가
    - Auth 도메인 Swagger & JavaDocs 문서화 추가
    - User 도메인 Swagger & JavaDocs 문서화 추가

--- src/main/java/run/bemin/api/auth/controller/SignupController.java
+++ src/main/java/run/bemin/api/auth/controller/SignupController.java
@@ -1,5 +1,7 @@
 package run.bemin.api.auth.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -19,27 +21,39 @@ import run.bemin.api.auth.dto.response.SignupResponseDto;
 import run.bemin.api.auth.service.AuthService;
 import run.bemin.api.general.response.ApiResponse;
 
+/**
+ * SignupController는 사용자 회원가입 및 중복 확인 관련 API 엔드포인트를 제공합니다.
+ */
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/api/auth")
+@Tag(name = "회원가입", description = "사용자 회원가입 및 중복 확인 컨트롤러")
 public class SignupController {
 
   private final AuthService authService;
 
   /**
-   * 회원가입
+   * 사용자 회원가입 API.
+   *
+   * @param requestDTO 회원가입 요청 DTO (필요한 사용자 정보 포함)
+   * @return 회원가입 결과와 함께 생성된 사용자 정보를 반환합니다.
    */
   @PostMapping("/signup")
+  @Operation(summary = "회원가입", description = "새로운 사용자를 등록합니다.")
   public ApiResponse<SignupResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDTO) {
     SignupResponseDto responseDto = authService.signup(requestDTO);
     return ApiResponse.from(HttpStatus.OK, "성공", responseDto);
   }
 
   /**
-   * 이메일 중복 확인
+   * 이메일 중복 확인 API.
+   *
+   * @param email 확인할 이메일 주소
+   * @return 이메일 중복 여부와 관련 메시지를 반환합니다.
    */
   @GetMapping("/email/exists")
+  @Operation(summary = "이메일 중복 확인", description = "이메일의 중복 여부를 확인합니다.")
   public ResponseEntity<ApiResponse<EmailCheckResponseDto>> checkEmail(
       @RequestParam @NotBlank(message = "이메일을 입력해주세요.") String email
   ) {
@@ -53,11 +67,14 @@ public class SignupController {
     return ResponseEntity.ok(response);
   }
 
-
   /**
-   * 닉네임 중복 확인
+   * 닉네임 중복 확인 API.
+   *
+   * @param nickname 확인할 닉네임
+   * @return 닉네임 중복 여부와 관련 메시지를 반환합니다.
    */
   @GetMapping("/nickname/exists")
+  @Operation(summary = "닉네임 중복 확인", description = "닉네임의 중복 여부를 확인합니다.")
   public ResponseEntity<ApiResponse<NicknameCheckResponseDto>> checkNickname(
       @RequestParam @NotBlank(message = "닉네임을 입력해주세요.") String nickname
   ) {
