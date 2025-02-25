88e3bf39fa7220ae87c1849169f4dbd981f442f7
LimDae94 <piay801@naver.com>
2025-02-25 11:29:27 +0900

    Fix: #128 Fetch Join 으로 가게 리뷰 갖고 오기
    Fetch Join 으로 가게 리뷰 갖고 오기

--- src/main/java/run/bemin/api/store/repository/StoreRepository.java
+++ src/main/java/run/bemin/api/store/repository/StoreRepository.java
@@ -1,10 +1,13 @@
 package run.bemin.api.store.repository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import run.bemin.api.store.entity.Store;
 
 public interface StoreRepository extends JpaRepository<Store, UUID>, StoreRepositoryCustom {
@@ -17,5 +20,8 @@ public interface StoreRepository extends JpaRepository<Store, UUID>, StoreReposi
 
   // 삭제 여부 상관 없이 검색
   Page<Store> findAllByNameContainingIgnoreCase(String name, Pageable pageable);
+
+  @Query("select s from p_store s left join fetch s.reviews where s.id = :storeId")
+  Optional<Store> findByIdWithReviews(@Param("storeId") UUID storeId);
 
 }
